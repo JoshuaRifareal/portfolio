c650dc4ff69e5d304723c7aa4366add07ce663ac
Joshua Rifareal <joshrifareal123@gmail.com>
2021-09-10 15:24:25 +0800

    Purge
    For reuploading

--- zoomBlur.js
+++
@@ -1,7 +0,0 @@
-var scrollPos = 0;
-
-document.addEventListener('scroll', function(e) {
-    scrollPos = $(this).scrollTop();
-    document.getElementById("portrait-container").style.filter = "blur("+ scrollPos*0.01 + "px)";
-});
-
